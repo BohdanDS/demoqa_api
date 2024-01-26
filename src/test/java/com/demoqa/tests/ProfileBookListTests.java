08b6bab3696844347ffcff6c2569b29fb1c55920
bohdan <bohdanpeliutkevich@gmail.com>
2024-01-26 13:28:38 +0300

    add Allure steps

--- src/test/java/com/demoqa/tests/ProfileBookListTests.java
+++ src/test/java/com/demoqa/tests/ProfileBookListTests.java
@@ -18,6 +18,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
 
 public class ProfileBookListTests extends TestBase {
 
@@ -25,7 +26,7 @@ public class ProfileBookListTests extends TestBase {
 
     @Test
     @DisplayName("Check that specified book was added to user list")
-    void addBookToProfileTest(){
+    void addBookToProfileTest() {
         LoginResponseModel authResponse = AuthorizationApi.Login();
 
         List<IsbnModel> isbns = new ArrayList<>();
@@ -46,9 +47,9 @@ public class ProfileBookListTests extends TestBase {
 
     @Test
     @DisplayName("Delete specified book from user list")
-    void deleteSpecifiedBookById(){
-        LoginResponseModel authResponse = AuthorizationApi.Login();
-        BookApi.deleteAllBooks(authResponse.getUserId(), authResponse.getToken());
+    void deleteSpecifiedBookById() {
+        LoginResponseModel authResponse = step("Login API request", AuthorizationApi::Login);
+        step("Remove all books from user list", () -> BookApi.deleteAllBooks(authResponse.getUserId(), authResponse.getToken()));
 
         List<IsbnModel> isbns = new ArrayList<>();
         isbns.add(new IsbnModel(isbnId));
@@ -58,20 +59,26 @@ public class ProfileBookListTests extends TestBase {
         deleteBook.setUserId(authResponse.getUserId());
         deleteBook.setIsbn(isbnId);
 
-        BookApi.addBookToCard(addBookRequest, authResponse.getToken());
+        step("Add specified book to user List", () -> BookApi.addBookToCard(addBookRequest, authResponse.getToken()));
 
         open("/favicon.ico");
         getWebDriver().manage().addCookie(new Cookie("expires", authResponse.getExpires()));
         getWebDriver().manage().addCookie(new Cookie("userID", authResponse.getUserId()));
         getWebDriver().manage().addCookie(new Cookie("token", authResponse.getToken()));
 
-        open("/profile");
-        $(".ReactTable").shouldHave(text("Learning JavaScript Design Patterns"));
+        step("Checks that added book is displayed in user profile", () ->
+                {
+                    open("/profile");
+                    $(".ReactTable").shouldHave(text("Learning JavaScript Design Patterns"));
+                }
+        );
 
-        BookApi.deleteBookFromCartById(deleteBook, authResponse.getToken());
+        step("Remove specified book by Id from user profile", () -> BookApi.deleteBookFromCartById(deleteBook, authResponse.getToken()));
 
-        Selenide.refresh();
-        $(".rt-noData").shouldHave(text("No rows found"));
+        step("Check that removed books is not displayed in user profile",()->{
+            Selenide.refresh();
+            $(".rt-noData").shouldHave(text("No rows found"));
+        });
 
     }
 
